58795ac14dc86e76dcf87c305e1dd7e65a3160d4
Ben Carpenter <benjamincarp@gmail.com>
2023-10-20 10:52:24 -0500

    fetching data

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import { draftMode } from 'next/headers'
+import { fetchRecipes } from '../contentful/recipes'
 
-export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  export default async function Home() {
+    const recipeList = await fetchRecipes({ preview: draftMode().isEnabled });
+
+    console.log(recipeList);
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>
+          MixUp
+        </h1>
+      </main>  
+    )
+  }
