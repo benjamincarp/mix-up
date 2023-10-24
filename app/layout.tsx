38f8ccde829b1394b4eedb69a6c0849f51c50ec4
Ben Carpenter <benjamincarp@gmail.com>
2023-10-24 10:39:24 -0500

    add draft mode header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { draftMode } from 'next/headers'
+import ExitDraftModeLink from './ExitDraftModeLink'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {draftMode().isEnabled && (
+            <p className="bg-orange-200 py-4 px-[6vw]">
+              Draft mode is on! <ExitDraftModeLink className="underline" />
+            </p>
+          )}
+        {children}
+      </body>
     </html>
   )
 }
