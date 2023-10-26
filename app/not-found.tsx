be8c5e0de169b01f171e039a32baf52511e5894d
Ben Carpenter <benjamincarp@gmail.com>
2023-10-25 23:46:09 -0500

    escape an appostrophe

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <main>
-      <h1>Sorry, I don't know that one.</h1>
+      <h1>Sorry, I don&apos;t know that one.</h1>
       <Link href="/">Return Home</Link>
     </main>
   )
