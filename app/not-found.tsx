0f12028f6ab3620bb29ae7905116f938f3c689fc
Ben Carpenter <benjamincarp@gmail.com>
2023-10-26 17:00:12 -0500

    style 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import ContentCard from './components/ContentCard'
  
 export default function NotFound() {
   return (
     <main>
-      <h1>Sorry, I don&apos;t know that one.</h1>
-      <Link href="/">Return Home</Link>
+      <ContentCard titleText='404' >
+        <div className='text-center'>
+          Sorry, I don&apos;t know that one.
+          <br />
+          Clink the glass below to go back home.
+        </div>
+      </ContentCard>
     </main>
   )
 }
