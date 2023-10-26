67f2c4352f8f4581af2b720f78adb8db1f250736
Ben Carpenter <benjamincarp@gmail.com>
2023-10-26 15:45:25 -0500

    now even more stylish

--- app/components/Separator.tsx
+++ app/components/Separator.tsx
@@ -1,7 +1,7 @@
 export default function Separator () {
     return (
         <span className='flex flex-row justify-center'>
-            <hr className='w-14 text-center'/>
+            <hr className='w-14 border-px border-single text-center '/>
         </span>
     );
 }
