f1939c76f3f31566c470ffe5f356a379c8f08aaa
Ben Carpenter <benjamincarp@gmail.com>
2023-10-27 21:37:20 -0500

    remove unneeded files

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'MixUp',
-  description: 'Cocktails Curated',
-  manifest: '/site.webmanifest'
+  description: 'Cocktails Curated'
 }
 
 export default function RootLayout({
