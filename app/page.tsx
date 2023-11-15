fe40c4f5d96d94487319440970f74f90a12be21f
Ben Carpenter <benjamincarp@gmail.com>
2023-11-15 12:03:37 -0600

    configurabe home-page menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 import { draftMode } from "next/headers";
-import { Section, fetchSections } from "../contentful/menu";
+import { Menu, fetchActiveMenu, Section, fetchSections } from "../contentful/menu";
 import RecipeList from "../components/RecipeList";
 
 export default async function Home() {
-  const catList = await fetchSections();
-  console.log('Categories')
-  console.dir(catList)
-
-  return catList.map((section:Section) => (<RecipeList key={section.id} title={section.name} recipeList={section.drinks} />))
+  const menu = await fetchActiveMenu();
+  return menu?.sections.map((section:Section) => (<RecipeList key={section.id} title={section.name} recipeList={section.drinks} />))
 }
