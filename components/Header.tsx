0e566da5bd2615ea284c0fd17c8286e1753dbe7e
Ben Carpenter <benjamincarp@gmail.com>
2024-05-15 16:25:12 -0500

    fix header when the title wraps to multiple lines

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,8 @@ interface HeaderParams {
 
 export default function Header ({text}: HeaderParams){
     return (<h1 className='flex flex-row justify-center m-3 mb-6 text-3xl'>
-        <div className='p-1 border border-double inline'>
-            <div className='p-2.5 border border-double inline'>
+        <div className='p-1 border border-double'>
+            <div className='p-3 border border-double -mt-2.5 -mb-2.5 text-center'>
                 {text.toUpperCase()}
             </div>
         </div>
